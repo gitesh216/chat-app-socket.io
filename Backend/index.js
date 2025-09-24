ddf1e9eafe7aabd1934aaf47e721238d62f2cf1b
gitesh216 <109015479+gitesh216@users.noreply.github.com>
2025-09-25 00:24:11 +0530

    feat: user join room event listen

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ import { Server } from "socket.io";
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = 4600;
 
 const server = createServer(app);
 
@@ -13,14 +13,29 @@ const io = new Server(server, {
     }
 });
 
+
+const ROOM = "group";
+
 io.on("connection", (socket) => {
     console.log("User connected", socket.id);
+
+    socket.on("joinRoom", async (userName) => {
+        console.log(`${userName} joined to group!`);
+
+        await socket.join(ROOM);
+
+        // send to all users
+        // io.to(ROOM).emit("roomNotice", userName);
+
+        // broadcast to all users
+        socket.to(ROOM).emit("roomNotice", userName);
+    });
 });
 
 app.get("/", (req, res) => {
     res.send("HEllo world");
 });
 
-app.listen(port, ()=>{
+server.listen(port, ()=>{
     console.log("Server listening on port", port);
 });
